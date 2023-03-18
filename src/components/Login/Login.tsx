1c4eb19486870158fa2713b9ab37c2a513c45034
MalevichDi2910 <malevicdiana@gmail.com>
2023-03-18 10:55:01 +0300

    edit form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,15 +1,25 @@
-import React, {useState} from "react";
+import React from "react";
 import {FieldValues, useForm} from "react-hook-form";
+import {connect} from "react-redux";
+import {loginTC} from "../../redux/auth-reducer";
+import {Navigate} from "react-router-dom";
+import {AppStateType} from "../../redux/redux-store";
 
-const Login = () => {
+type LoginPropsType = {
+    loginTC: (email: string, password: string, rememberMe: boolean) => void
+    isAuth: boolean
+}
+
+const Login = (props: LoginPropsType) => {
     const {register, handleSubmit, formState: {errors}, reset} = useForm();
 
-    const [data, setData] = useState("");
-
     const onSubmit = (data: FieldValues) => {
-        setData(JSON.stringify(data));
-        console.log(data);
+        props.loginTC(data.email, data.password, data.rememberMe)
         reset();
+    }
+
+    if(props.isAuth) {
+        return <Navigate to={'/profile'}/>
     }
 
     return (
@@ -52,12 +62,20 @@ const Login = () => {
             </div>
             {errors.password && <span role="alert">{errors.password.message?.toString()}</span>}
             <div>
-                <input {...register("remember me")} type='checkbox'/> remember me
+                <input {...register("rememberMe")} type='checkbox'/> remember me
             </div>
             <button type="submit">Log in</button>
         </form>
     );
 }
 
-export default Login;
+type MapStateToPropsType = {
+    isAuth: boolean
+}
+
+const mapStateToProps = (state: AppStateType): MapStateToPropsType => ({
+        isAuth: state.auth.isAuth
+})
+
+export default connect (mapStateToProps, {loginTC}) (Login);
 
