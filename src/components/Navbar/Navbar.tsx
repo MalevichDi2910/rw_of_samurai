4fba279fd5c24f1369e8550e1017c4c1f54891c4
MalevichDi2910 <malevicdiana@gmail.com>
2023-03-18 10:59:44 +0300

    edit Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -12,44 +12,45 @@ const Navbar = () => {
     ]
 
   let friendsNav = friends.map(f => {
-    return (
-        <div className={s.nameFriend}>
-          <img
-              src='https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745'
-              alt={f.name}/>{f.name}
+      return (
+          <div className={s.nameFriend}>
+              <img
+                  src='https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745'
+                  alt={f.name}/>
+              {f.name}
           </div>
-          )
-          })
+      )
+  })
 
-                return(
-                    <nav className={s.nav}>
-                        <div className={s.item}>
-                            <NavLink to="/profile"> Profile </NavLink>
-                        </div>
-                        <div className={`${s.item}`}>
-                            <NavLink to="/dialogs"> Messages </NavLink>
-                        </div>
-                        <div className={`${s.item}`}>
-                        <NavLink to="/users"> Users </NavLink>
-                        </div>
-                        <div className={s.item}>
-                            <NavLink to="/"> News </NavLink>
-                        </div>
-                        <div className={s.item}>
-                            <NavLink to="/"> Music </NavLink>
-                        </div>
-                        <div className={s.item}>
-                            <NavLink to="/"> Settings </NavLink>
-                        </div>
-                        <hr/>
-                        <div className={s.friends}>
-                            <div className={s.nameSelection}>Friends</div>
-                            <div className={s.infoFriends}>
-                                {friendsNav}
-                            </div>
-                        </div>
-                    </nav>
-                )
-                };
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to="/profile"> Profile </NavLink>
+            </div>
+            <div className={`${s.item}`}>
+                <NavLink to="/dialogs"> Messages </NavLink>
+            </div>
+            <div className={`${s.item}`}>
+                <NavLink to="/users"> Users </NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/"> News </NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/"> Music </NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/"> Settings </NavLink>
+            </div>
+            <hr/>
+            <div className={s.friends}>
+                <div className={s.nameSelection}>Friends</div>
+                <div className={s.infoFriends}>
+                    {friendsNav}
+                </div>
+            </div>
+        </nav>
+    )
+};
 
-                export default Navbar;
+export default Navbar;
