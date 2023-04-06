e33e19f6d8f27e72321b086d98528ff029d1dbd2
MalevichDi2910 <malevicdiana@gmail.com>
2023-04-06 22:06:35 +0300

    add component User

---
+++ src/components/Users/User.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import s from "./Users.module.css";
+import {UserType} from "../../redux/users-reducer";
+import userPhoto from "../../assets/images/user.png";
+import {NavLink} from "react-router-dom";
+import Paginator from "../common/Paginator/Paginator";
+
+type UsersProps = {
+    user: UserType
+    followingInProgress: number[]
+    follow: (userId: number) => void
+    unfollow: (userId: number) => void
+}
+
+const User = ({user, followingInProgress, unfollow, follow}: UsersProps) => {
+
+    return (
+        <div>
+            <span>
+                <div>
+                    <NavLink to={'/profile/' + user.id}>
+                        <img src={user.photos.small != null ? user.photos.small : userPhoto}
+                                 className={s.userPhoto}/>
+                    </NavLink>
+                </div>
+                <div>
+                    {user.followed
+                        ? <button disabled={followingInProgress.some(id => id === user.id)}
+                                  onClick={() => {
+                                      unfollow(user.id)
+                                  }}> UnFollow </button>
+
+                        : <button disabled={followingInProgress.some(id => id === user.id)}
+                                  onClick={() => {
+                                      follow(user.id)
+                                  }}>Follow</button>}
+                </div>
+            </span>
+            <span>
+                <span>
+                    <div>{user.name}</div>
+                    <div>{user.status}</div>
+                </span>
+                <span>
+                    <div>{user.location?.city}</div>
+                    <div>{user.location?.country}</div>
+                </span>
+            </span>
+        </div>
+    )
+}
+
+export default User;
