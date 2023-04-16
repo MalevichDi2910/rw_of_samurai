f86bd972f3f5dc99633411adbd4ec8d31b73b540
MalevichDi2910 <malevicdiana@gmail.com>
2023-04-16 20:00:09 +0300

    change Paginator

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,31 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
 import s from "./Paginator.module.css";
 
 type PaginatorProps = {
     currentPage: number
     pageSize: number
-    totalUsersCount: number
+    totalItemsCount: number
     onPageChanged: (pageNumber: number) => void
+    portionSize: number
 }
 
-const Paginator = ({currentPage, totalUsersCount, onPageChanged, pageSize}: PaginatorProps) => {
+const Paginator = ({currentPage, totalItemsCount, onPageChanged, pageSize, portionSize = 10}: PaginatorProps) => {
 
-    let pagesCount = Math.ceil(totalUsersCount / pageSize)
+    let pagesCount = Math.ceil(totalItemsCount / pageSize)
 
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
 
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let[portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
+
     return (
-        <div>
-            {pages.map(p => {
+        <div className={s.paginator}>
+            { portionNumber > 1 &&
+                <button onClick={ () => { setPortionNumber(portionNumber - 1) }}>PREV</button>
+            }
+
+            {pages
+                .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                .map(p => {
                 return <span className={currentPage === p && s.selectedPage}
+                             key={p}
                              onClick={(e) => {
                                  onPageChanged(p)
                              }}>{p}</span>
             })}
+
+            {portionCount > portionNumber &&
+                <button onClick={ () => { setPortionNumber(portionNumber + 1) }}>NEXT</button>
+            }
+
         </div>
+
+
+
+
+
+
     )
 }
 
