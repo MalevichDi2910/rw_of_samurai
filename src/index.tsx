071bdcb05e6130ac61d1ad882d82621d15c4f0b0
MalevichDi2910 <malevicdiana@gmail.com>
2023-04-16 20:02:07 +0300

    change index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,12 @@
 import reportWebVitals from "./reportWebVitals";
-import store from "./redux/redux-store";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import "./index.css";
-import App from "./App";
-import {Provider} from "react-redux";
-import {HashRouter} from "react-router-dom";
+import {SamuraiJSApp} from "./App";
 
-const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-    root.render(
-        <React.StrictMode>
-            <HashRouter>
-                <Provider store={store}>
-                    <App/>
-                </Provider>
-            </HashRouter>
-        </React.StrictMode>
-    );
+root.render(<SamuraiJSApp/>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
