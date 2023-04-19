4858355e2d280221b7ffb825c14242a105ca8014
MalevichDi2910 <malevicdiana@gmail.com>
2023-04-19 20:22:16 +0300

    change UsersContainer

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -8,7 +8,7 @@ import {compose} from "redux";
 import {
     getCurrentPage, getFollowingInProgress,
     getIsFetching,
-    getPageSize,
+    getPageSize, getPortionSize,
     getTotalUsersCount, getUsers
 } from "../../redux/users-selectors";
 
@@ -27,10 +27,11 @@ class UsersContainer extends React.Component<UsersAPIComponentPropsType>{
     render() {
         return <>
             {this.props.isFetching ? <Preloader/> : null}
-            <Users totalUsersCount={this.props.totalUsersCount}
+            <Users totalItemsCount={this.props.totalItemsCount}
                    pageSize={this.props.pageSize}
                    currentPage={this.props.currentPage}
                    onPageChanged={this.onPageChanged}
+                   portionSize={this.props.portionSize}
                    users={this.props.users}
                    follow={this.props.follow}
                    unfollow={this.props.unfollow}
@@ -42,10 +43,11 @@ class UsersContainer extends React.Component<UsersAPIComponentPropsType>{
 type MapStateToPropsType = {
     users: Array<UserType>
     pageSize: number
-    totalUsersCount: number
+    totalItemsCount: number
     currentPage: number
     isFetching: boolean
     followingInProgress: number[]
+    portionSize: number
 }
 
 type MapDispatchToPropsType = {
@@ -61,10 +63,11 @@ function mapStateToProps(state: AppStateType): MapStateToPropsType {
     return {
         users: getUsers(state),
         pageSize: getPageSize(state),
-        totalUsersCount: getTotalUsersCount(state),
+        totalItemsCount: getTotalUsersCount(state),
         currentPage: getCurrentPage(state),
         isFetching: getIsFetching(state),
-        followingInProgress: getFollowingInProgress(state)
+        followingInProgress: getFollowingInProgress(state),
+        portionSize: getPortionSize(state)
     }
 }
 
