1719757b724eb5ff4ce630c3b5cbd385c71b6802
MalevichDi2910 <malevicdiana@gmail.com>
2023-04-24 15:44:26 +0300

    change Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import ProfileInfo, {ProfileType} from "./ProfileInfo/ProfileInfo";
+import ProfileInfo, {PhotosType, ProfileType} from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
-const Profile = (props: { profile: ProfileType, status: string, updateStatus: (status: string) => void }) => {
+type ProfileProps = {
+    profile: ProfileType
+    isOwner: boolean
+    status: string
+    updateStatus: (status: string) => void
+    savePhoto: (file: File) => void
+}
+
+const Profile = (props: ProfileProps) => {
     return (
         <div>
-            <ProfileInfo profile={props.profile} status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileInfo profile={props.profile} isOwner={props.isOwner} status={props.status}
+                         updateStatus={props.updateStatus} savePhoto={props.savePhoto}/>
             <MyPostsContainer/>
         </div>
     );
