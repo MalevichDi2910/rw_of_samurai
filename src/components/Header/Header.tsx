ac650cea155118ce8bdee11f479c663605698eb6
MalevichDi2910 <malevicdiana@gmail.com>
2024-03-09 17:56:20 +0200

    fix: fix style for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,12 +6,12 @@ import {MapStateToPropsType} from "./HeaderContainer";
 const Header = (props: MapStateToPropsType & {logoutTC:() => void} ) => {
     return <>
         <header className={s.header}>
-            <img alt='' src='https://i.pinimg.com/474x/d3/0a/19/d30a19c7f5c494218deb98202f3f6422.jpg'/>
+            <span className={s.logo}><img alt='' src='https://cryptologos.cc/logos/polygon-matic-logo.png'/></span>
 
             <div className={s.loginBlock}>
                 {
                     props.isAuth
-                        ? <div>{props.login} - <button onClick={props.logoutTC}>Log out</button></div>
+                        ? <div>{props.login} - <button className={s.btn} onClick={props.logoutTC}>Log out</button></div>
                         : <NavLink to={'/login'}>Login</NavLink>
                 }
             </div>
