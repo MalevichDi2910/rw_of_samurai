5c364e68f1ac83567cf1e8cc87db78e11a8bc162
MalevichDi2910 <malevicdiana@gmail.com>
2024-03-16 16:48:15 +0200

    fix: fix User

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -14,37 +14,35 @@ type UsersProps = {
 const User = ({user, followingInProgress, unfollow, follow}: UsersProps) => {
 
     return (
-        <div>
-            <span>
+        <div className={s.wrapper}>
                 <div>
                     <NavLink to={'/profile/' + user.id}>
                         <img src={user.photos.small != null ? user.photos.small : userPhoto}
                                  className={s.userPhoto}/>
                     </NavLink>
                 </div>
+                <div className={s.name}>
+                     {user.name}
+                </div>
                 <div>
                     {user.followed
-                        ? <button disabled={followingInProgress.some(id => id === user.id)}
+                        ? <button className={s.btnFollow} disabled={followingInProgress.some(id => id === user.id)}
                                   onClick={() => {
                                       unfollow(user.id)
                                   }}> UnFollow </button>
 
-                        : <button disabled={followingInProgress.some(id => id === user.id)}
+                        : <button className={s.btnFollow} disabled={followingInProgress.some(id => id === user.id)}
                                   onClick={() => {
                                       follow(user.id)
                                   }}>Follow</button>}
                 </div>
-            </span>
-            <span>
-                <span className={s.info}>
-                    <div>{user.name}</div>
-                    <div>{user.status}</div>
-                </span>
-                <span>
+                <div className={s.status}>
+                    {user.status}
+                </div>
+                <div className={s.info}>
                     <div>{user.location?.city}</div>
                     <div>{user.location?.country}</div>
-                </span>
-            </span>
+                </div>
         </div>
     )
 }
