eb54a1352702831766ca023497d2f4482f7f6ce0
MalevichDi2910 <malevicdiana@gmail.com>
2024-03-21 11:48:32 +0200

    style: add container

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {UserType} from "../../redux/users-reducer";
 import Paginator from "../common/Paginator/Paginator";
 import User from "./User";
+import s from './Users.module.css'
 
 type UsersProps = {
     currentPage: number
@@ -29,12 +30,15 @@ const Users = ({currentPage, totalItemsCount, onPageChanged, pageSize, users, po
         <div>
             <Paginator currentPage={currentPage} pageSize={pageSize} totalItemsCount={totalItemsCount}
                        onPageChanged={onPageChanged} portionSize={portionSize}/>
-            {users.map((u: UserType) => <User key={u.id}
-                                              user={u}
-                                              followingInProgress={props.followingInProgress}
-                                              unfollow={props.unfollow}
-                                              follow={props.follow}/>
-            )}
+            <div className={s.container}>
+                {users.map((u: UserType) => <User key={u.id}
+                                                  user={u}
+                                                  followingInProgress={props.followingInProgress}
+                                                  unfollow={props.unfollow}
+                                                  follow={props.follow}/>
+                )}
+            </div>
+
         </div>
     )
 }
