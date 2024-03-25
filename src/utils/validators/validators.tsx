1d71604eeb83a21d88ccad463f874d63d5d4c565
MalevichDi2910 <malevicdiana@gmail.com>
2024-03-25 13:52:10 +0200

    feat: add validators

--- src/utils/validators/validators.tsx
+++ src/utils/validators/validators.tsx
@@ -1,9 +1,11 @@
-export const required = (value: any) => {
+export type FieldValidatorType = (value: string) => string | undefined
+
+export const required = (value: string) => {
     if (value) return undefined
     return 'Field is required'
 }
 
-export const maxLengthCreator = (maxLength: number) => (value: any) => {
+export const maxLengthCreator = (maxLength: number) => (value: string) => {
     if (value.length > maxLength ) return `Max length is ${maxLength} symbols`
     return undefined;
 }
