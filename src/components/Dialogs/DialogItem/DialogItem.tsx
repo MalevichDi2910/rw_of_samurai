a08b980c4045df0b82b9b87b4999d6d02cb70e29
MalevichDi2910 <malevicdiana@gmail.com>
2024-03-30 13:10:17 +0200

    fix: fix DialogItem

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -10,9 +10,11 @@ type DialogItemProps = {
 const DialogItem = (props: DialogItemProps) => {
     let path = '/dialogs/' + props.id;
 
-    return <div className={s.dialog + ' ' + s.active}>
-        <img alt='' src='https://cdn-icons-png.flaticon.com/512/3003/3003035.png'></img>
-        <NavLink to={path}>{props.name}</NavLink>
+    return <div className={s.dialog}>
+        <img
+            src="https://t4.ftcdn.net/jpg/04/83/90/87/360_F_483908734_Fn5m2DUij8ijZHgD95HgMLty2kOVZ7hA.png"
+            alt="avatar"/>
+        <NavLink to={path} className={s.navName}>{props.name}</NavLink>
     </div>
 }
 
