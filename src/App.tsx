1802993c1282241ba770fbb59783e7b04c75126e
MalevichDi2910 <malevicdiana@gmail.com>
2024-04-09 18:25:01 +0200

    feat: add new Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {ComponentType} from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import {HashRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import {withRouter} from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
@@ -11,9 +11,13 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import store, {AppStateType} from "./redux/redux-store";
 import Preloader from "./components/common/Preloader/Preloader";
+import {ErrorPage} from "../src/components/common/ErrorPage/ErrorPage";
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const MusicContainer = React.lazy(() => import('./components/Music/Music'));
+const NewsContainer = React.lazy(() => import('./components/News/News'));
+const SettingsContainer = React.lazy(() => import('./components/Settings/Settings'));
 
 class App extends React.Component<ownPropsType> {
 
@@ -33,12 +37,18 @@ class App extends React.Component<ownPropsType> {
                 <div className={"app-wrapper-content"}>
                     <React.Suspense fallback={<Preloader/>}>
                         <Routes>
-                            <Route path={"/dialogs"} element={<DialogsContainer/>}/>
+
+                            <Route element={<Navigate to='/profile'/>} path={'/'}/>
+                            <Route path={"/dialogs/*"} element={<DialogsContainer/>}/>
                             <Route path={"/profile"} element={<ProfileContainer/>}>
                                 <Route path={':userId'} element={<ProfileContainer/>}/>
                             </Route>
                             <Route path={"/users"} element={<UsersContainer/>}/>
                             <Route path={"/login"} element={<Login/>}/>
+                            <Route path={"/music"} element={<MusicContainer/>}/>
+                            <Route path={"/news"} element={<NewsContainer/>}/>
+                            <Route path={"/settings"} element={<SettingsContainer/>}/>
+                            <Route path={"*"} element={<ErrorPage/>}/>
                         </Routes>
                     </React.Suspense>
                 </div>
