b946a6c884f4ecfed7d0f54cb9f4fcdd3a25f076
MalevichDi2910 <malevicdiana@gmail.com>
2024-04-14 18:13:57 +0200

    feat: add name and img

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -3,13 +3,23 @@ import s from "./../Dialogs.module.css"
 
 type MessagePropsType = {
     message: string
+    name:string
 }
 
 const Message = (props: MessagePropsType) => {
 
     return (
         <>
-        <div className={s.message}>{props.message}</div>
+                <div className={s.message}>
+                    <img
+                        src='https://t4.ftcdn.net/jpg/04/83/90/87/360_F_483908734_Fn5m2DUij8ijZHgD95HgMLty2kOVZ7hA.png'
+                        alt={'logo'} className={s.image}/>
+
+                    <div className={s.textWrap}>
+                            <div className={s.name}>{props.name}</div>
+                            <div className={s.text}>{props.message}</div>
+                    </div>
+                </div>
         </>
     )
 }
