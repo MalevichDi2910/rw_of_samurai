f956e0cef47cb97ab498a68c0863cfcfbadc2b6e
MalevichDi2910 <malevicdiana@gmail.com>
2024-04-14 18:11:03 +0200

    fix: fix FormControl

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,23 +1,35 @@
 import React from 'react';
+import {Field, WrappedFieldProps} from 'redux-form';
+import {FieldValidatorType} from "../../../utils/validators/validators";
+import s from './FormsControls.module.css'
 
-export const FormControl = ({input, meta: {touched, error}, children}: any) => {
+export const FormControl = ({meta: {touched, error}, children}: any) => {
     const hasError = touched && error;
     return (
-        <div>
-            <div>
-                {children}
-            </div>
-            { hasError && <span>{ error}</span> }
+        <div className={s.formControl + ' ' + (hasError ? s.error : '')}>
+            <div>{children}</div>
+            { hasError && <span>{error}</span> }
         </div>
     );
 };
 
-export const Textarea = (props: any) => {
-    const {input, meta, child, ...restProps} = props;
-    return <FormControl {...props}><textarea {...input} {...restProps}/></FormControl>
+export const Textarea: React.FC<WrappedFieldProps> = (props) => {
+    const {input, meta, ...restProps} = props;
+    return <FormControl {...props}><textarea {...input} {...restProps} className={s.textArea}/></FormControl>
 };
 
-export const Input = (props: any) => {
-    const {input, meta, child, ...restProps} = props;
-    return <FormControl {...props}><input {...input} {...restProps}/></FormControl>
+export const Input: React.FC<WrappedFieldProps> = (props) => {
+    const {input, meta, ...restProps} = props;
+    return <FormControl {...props}><input {...input} {...restProps} className={s.inputText}/></FormControl>
 };
+
+export const createField = (placeholder: string, name: string, validators: FieldValidatorType[], component: React.FC<WrappedFieldProps>, props = {}, text = '') => {
+    return <div style={{display: 'flex', gap: '10px', justifyContent: 'center', alignItems: 'center', color: 'black'}}>
+        <Field placeholder={placeholder}
+               name={name}
+               validate={validators}
+               component={component}
+               {...props}/>
+        {text}
+    </div>
+}
