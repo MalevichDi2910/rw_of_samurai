2f6da3f1ccbbca811dc5993172f6c30631b696a2
MalevichDi2910 <malevicdiana@gmail.com>
2024-04-16 17:16:56 +0200

    feat: add profile

--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -1,23 +1,23 @@
-import React from "react";
 import { addPostActionCreator, PostType } from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
 import {connect} from "react-redux";
 import {AppStateType} from "../../../redux/redux-store";
 import {Dispatch} from "redux";
+import {ProfileType} from "./../ProfileInfo/ProfileInfo";
 
 type mapStateToPropsType = {
-    posts: Array<PostType>
+    posts: PostType[]
+    profile: ProfileType
 }
 
 type MapDispatchPropsType = {
     addPost: (newPostText: string) => void
 }
 
-const mapStateToProps = (state: AppStateType): mapStateToPropsType => {
-    return {
-        posts: state.profilePage.posts
-    }
-}
+const mapStateToProps = (state: AppStateType): mapStateToPropsType => ({
+    posts: state.profilePage.posts,
+    profile: state.profilePage.profile!
+})
 
 const mapDispatchToProps = (dispatch: Dispatch): MapDispatchPropsType => {
     return {
