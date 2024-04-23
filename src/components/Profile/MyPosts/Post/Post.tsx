b75a70fc0b1f469f0063522e84773884c40855ed
MalevichDi2910 <malevicdiana@gmail.com>
2024-04-23 12:34:12 +0200

    feat: add new props

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import s from './Post.module.css'
+import userPhoto from "../../../../assets/images/user.png";
+import {ProfileType} from "./../../ProfileInfo/ProfileInfo";
 
 type PostPropsType = {
     message: string
     like: number
+    date: string
+    profile: ProfileType
 }
 
-const Post = (props: PostPropsType) => {
+const Post = ({message, like, date, profile}: PostPropsType) => {
     return (
         <div className={s.item}>
-            <img alt='' src='https://www.1zoom.ru/big2/856/329183-alexfas01.jpg'></img>
-            {props.message}
+            <img alt='' src={profile?.photos.large || userPhoto}></img>
+            <div className={s.date}>{date}</div>
+            <div className={s.message}>{message}</div>
             <div>
-                <span> {props.like} like</span>
+                <span className={s.like}>{like} like</span>
             </div>
         </div>
     )
