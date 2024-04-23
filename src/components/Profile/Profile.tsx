2c11466117fc51a93a09052cc8a2b5b8484163ca
MalevichDi2910 <malevicdiana@gmail.com>
2024-04-23 12:35:35 +0200

    feat: add ProfileDataForm and ProfileData

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,8 @@
-import React from "react";
-import ProfileInfo, {ProfileType} from "./ProfileInfo/ProfileInfo";
+import React, {useState} from "react";
+import ProfileInfo, {ProfileData, ProfileType} from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import s from './Profile.module.css'
+import ProfileDataForm from "../Profile/ProfileInfo/ProfileDataForm";
 
 type ProfileProps = {
     profile: ProfileType
@@ -8,14 +10,34 @@ type ProfileProps = {
     status: string
     updateStatus: (status: string) => void
     savePhoto: (file: File) => void
+    saveProfile: (formData: ProfileType) => Promise<'pending' | 'fulfilled' | 'rejected'>
 }
 
-const Profile = (props: ProfileProps) => {
+const Profile = ({ profile, updateStatus, status, isOwner, savePhoto, saveProfile }: ProfileProps) => {
+    const[editMode, setEditMode] = useState(false);
+
+    const onSubmit = (formData: ProfileType) => {
+        saveProfile(formData).then(
+            () => {
+                setEditMode(false)
+            })
+    }
+
+    const goToEditMode = () => {
+        setEditMode(true)
+    }
+
     return (
         <div>
-            <ProfileInfo profile={props.profile} isOwner={props.isOwner} status={props.status}
-                         updateStatus={props.updateStatus} savePhoto={props.savePhoto}/>
-            <MyPostsContainer/>
+            <ProfileInfo profile={profile} isOwner={isOwner} status={status}
+                         updateStatus={updateStatus} savePhoto={savePhoto} saveProfile={saveProfile}/>
+            <div className={s.mainBlock}>
+                {editMode ?
+                    <ProfileDataForm initialValues={profile} profile={profile} onSubmit={onSubmit}/>:
+                    <ProfileData profile={profile} isOwner={isOwner} goToEditMode={goToEditMode} />
+                }
+                <MyPostsContainer/>
+            </div>
         </div>
     );
 };
